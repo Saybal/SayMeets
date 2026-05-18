6754e3dc54c6a768ea87ace726f237d7331e5442
Saybal <roysaybal@gmail.com>
2026-05-18 20:25:25 +0600

    Checking Host status

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -1,6 +1,17 @@
+'use client'
+import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
 import React from 'react'
 
 const EndCallButton = () => {
+    const call = useCall();
+    const { useLocalParticipant } = useCallStateHooks();
+    const participant = useLocalParticipant();
+
+    const isHost = participant && 
+        call?.state.createdBy &&
+        call?.state.createdBy.id === participant.userId
+    
+    if (!isHost) return null;
   return (
     <div>EndCallButton</div>
   )
